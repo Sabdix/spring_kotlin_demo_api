b9cb830ce35ab60546771887bc1c9edf02262582
Sabdi Pantoja <sabdi.pantoja@cmv.mx>
2023-05-27 14:44:38 -0600

    remaping and fix naming convention of tables

--- src/main/kotlin/com/example/demo/Controllers/CatalogController.kt
+++ src/main/kotlin/com/example/demo/Controllers/CatalogController.kt
@@ -5,10 +5,11 @@ import com.example.demo.Dtos.WsResponse
 import com.example.demo.Entities.SexoEntity
 import com.example.demo.Repositories.SexoRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-
+@RequestMapping("/catalog")
 class CatalogController(
         private val sexoRepository: SexoRepository
 ) {
